b4e4a1fab838ab94851f580123894fe012b123d3
Daniel MC <mitmacontreras@gmail.com>
2025-10-14 02:40:25 -0500

    CORS

--- src/main/java/Hotel/jwt/controller/InvoiceController.java
+++ src/main/java/Hotel/jwt/controller/InvoiceController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/invoices")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class InvoiceController {
 
--- src/main/java/Hotel/jwt/controller/RoomController.java
+++ src/main/java/Hotel/jwt/controller/RoomController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/rooms")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class RoomController {
     private final RoomService service;
 
